520c8be0403c52206984b88f11438e3218548b93
tslgithub <tiansonglu@gmail.com>
2025-01-07 14:53:32 +0800

    Update demo_imgs.py

--- Selective-RAFT/demo_imgs.py
+++ Selective-RAFT/demo_imgs.py
@@ -1,6 +1,6 @@
 import sys
 sys.path.append('core')
-import os
+import os,cv2
 os.environ['CUDA_VISIBLE_DEVICES'] = '0'
 
 import argparse
@@ -42,6 +42,9 @@ def demo(args):
             image1 = load_image(imfile1)
             image2 = load_image(imfile2)
 
+            left_cv_image = cv2.imread(imfile1)
+            right_cv_image = cv2.imread(imfile1)
+
             padder = InputPadder(image1.shape, divis_by=32)
             image1, image2 = padder.pad(image1, image2)
 
@@ -51,7 +54,31 @@ def demo(args):
             disp = padder.unpad(disp)
             file_stem = imfile1.split('/')[-2]
             filename = os.path.join(output_directory, f'{file_stem}.png')
-            plt.imsave(filename, disp.squeeze(), cmap='jet')
+            # plt.imsave(filename, disp.squeeze(), cmap='jet')
+            file_stem = imfile1.split('/')[-1]
+            name,end = file_stem.split(".")
+            filename = os.path.join(output_directory, name+"_depth."+end )
+
+            # 合并显示
+            disp = np.round(disp * 256).astype(np.uint16)
+            depth = cv2.applyColorMap(cv2.convertScaleAbs(disp.squeeze(), alpha=0.01),cv2.COLORMAP_JET)
+            depth = np.append(np.append(left_cv_image,right_cv_image,1),depth,1)
+
+            cv2.rectangle(depth,(0,0),  (160,70),(0,255,0),-1)
+            cv2.rectangle(depth,(640,0),(640+200,70),(0,255,0),-1)
+            cv2.rectangle(depth,(640+640,0),(640+640+210,70),(0,255,0),-1)
+            cv2.putText(depth,"left",  (20, 50), 5,3, (255,0,255),3)
+            cv2.putText(depth,"right", (0+640,50),5,3,(255,0,255),3)
+            cv2.putText(depth,"depth", (0+640+640,50),5,3,(255,0,255),3)
+            ih,iw,ic = depth.shape
+            cv2.line(depth,(int(iw/3),0),(int(iw/3),ih),(255,0,128),4,1 )
+            cv2.line(depth,(int(iw/3)*2,0),(int(iw/3)*2,ih),(255,0,128),4,2 )
+
+            cv2.imwrite(filename,depth , [int(cv2.IMWRITE_PNG_COMPRESSION), 0] )
+
+            cv2.imshow("depth",depth)
+            cv2.waitKey(1)
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -72,7 +99,8 @@ if __name__ == '__main__':
     parser.add_argument('--n_downsample', type=int, default=2, help="resolution of the disparity field (1/2^K)")
     parser.add_argument('--slow_fast_gru', action='store_true', help="iterate the low-res GRUs more frequently")
     parser.add_argument('--n_gru_layers', type=int, default=3, help="number of hidden GRU levels")
-    
+    parser.add_argument('--precision_dtype',type=str, default='float16', choices=['float16', 'bfloat16', 'float32'], help='Choose precision type: float16 or bfloat16 or float32')
+
     args = parser.parse_args()
 
     demo(args)
